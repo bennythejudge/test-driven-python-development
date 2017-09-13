5994a3e17ac076642a71dc19d3dce4f7aa5c5257
Benedetto Lo Giudice <logube@gmail.com>
2017-09-13 23:15:19 +0200

    Add new tests and refactor tests

--- src/stock-alerter/stock.py
+++ src/stock-alerter/stock.py
@@ -2,5 +2,26 @@
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price = None
+        self.price_history = []
+        # self.price = None
 
+
+    def update(self, timestamp, price):
+        if price < 0:
+           raise ValueError("price should not be negative")
+        self.price_history.append(price)
+
+    # mimics the price property which now no longer exists
+    # we've changed it to price_history
+    # accessing Stock.price will call this method
+    # price can be accessed as an attribute althoug it
+    # no longer exists as such
+    @property
+    def price(self):
+        return self.price_history[-1] \
+        if self.price_history else None
+
+    def is_increasing_trend(self):
+        return self.price_history[-3] < \
+               self.price_history[-2] < \
+               self.price_history[-1]
--- src/stock-alerter/tests/test_stock.py
+++ src/stock-alerter/tests/test_stock.py
@@ -1,10 +1,65 @@
 import unittest
 from stock import Stock
+from datetime import datetime
+
 
 class StockTest(unittest.TestCase):
+
+    def setUp(self):
+        self.goog = Stock('GOOG')
+
     def test_price_of_a_new_stock_class_should_be_None(self):
-      stock = Stock("GOOG")
-      self.assertIsNone(stock.price)
+        # stock = Stock("GOOG")
+        self.assertIsNone(self.goog.price)
+
+    def test_stock_update(self):
+        """An Update should set the price on the stock object
+        We will be using the `datetime` module for the timestamp
+        """
+        # goog = Stock("GOOG")
+        self.goog.update(datetime(2017, 9, 6), price=10)
+        self.assertEqual(10, self.goog.price)
+
+    def test_negative_price_should_throw_ValueError(self):
+        # goog = Stock("GOOG")
+        # try:
+        #     goog.update(datetime(2017, 9, 6), -1)
+        # except ValueError:
+        #     return
+        # self.assertRaises(ValueError, goog.update, datetime(2017, 9, 6), -1)
+        #self.fail("ValueError was not raised")
+        with self.assertRaises(ValueError):
+            self.goog.update(datetime(2017, 9, 6), -1)
+
+    def test_stock_price_should_give_the_latest_price(self):
+        # goog = Stock('GOOGLE')
+        self.goog.update(datetime(2017, 9, 6), price=10)
+        self.goog.update(datetime(2017, 9, 7), price=8.4)
+        self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
+
+class StockTrendTest(unittest.TestCase):
+    def setUp(self):
+        self.goog = Stock('GOOG')
+
+    # a helper method
+    def given_a_series_of_prices(self, prices):
+        timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12), \
+                      datetime(2014, 2, 13), datetime(2014, 2, 14)]
+        for timestamp, price in zip(timestamps, prices):
+            self.goog.update(timestamp, price)
+
+    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
+        self.given_a_series_of_prices([8, 10, 12])
+        self.assertTrue(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_decreases(self):
+        self.given_a_series_of_prices([12, 10, 8])
+        self.assertFalse(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_equal(self):
+        self.given_a_series_of_prices([1, 1, 1])
+        self.assertFalse(self.goog.is_increasing_trend())
+
 
 if __name__ == "__main__":
     unittest.main()
